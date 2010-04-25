2f8df58073318330c40242a07d3520b1bff90aba
saces <saces@freenetproject.org>
2010-04-26 00:07:09 +0200

    organize imports

--- bundles/http/src/main/java/com/buglabs/osgi/http/HttpServer.java
+++ bundles/http/src/main/java/com/buglabs/osgi/http/HttpServer.java
@@ -30,8 +30,6 @@ package com.buglabs.osgi.http;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 import javax.servlet.Servlet;
